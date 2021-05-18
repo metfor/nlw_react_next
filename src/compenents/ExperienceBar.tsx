35095df6364eff5ca9245e299f86e7dbcc49026f
metfor <mateus_frade@hotmail.com>
2021-05-18 19:43:05 -0300

    end 2

--- src/compenents/ExperienceBar.tsx
+++ src/compenents/ExperienceBar.tsx
@@ -1,10 +1,11 @@
+import styles from "../styles/components/ExperienceBar.module.css"
 export function ExperienceBar(){
     return(
-        <header className="experienceBar">
+        <header className={styles.experienceBar}>
            <span>0 xp</span> 
            <div>   
                <div style={{width: '50%'}}/>
-               <span className="current-experience"style={{left: "50%"}}>300 xp</span>
+               <span className={styles.currentExperience}style={{left: "50%"}}>300 xp</span>
            </div>
            <span>600 xp</span>
         </header>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,30 @@
-import Head from "next/head"
-import { ExperienceBar } from "../compenents/ExperienceBar";
 
+
+import { CompletedChallenges } from "../compenents/CompletedChallenges";
+import { Countdown } from "../compenents/Countdown";
+import { ExperienceBar } from "../compenents/ExperienceBar";
+import { Profile } from "../compenents/Profile";
+import styles from "../styles/pages/Home.module.css";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="container">
+    
+    <div className={styles.container}>
       <Head>
-      <link rel="preconnect" href="https://fonts.gstatic.com"/>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet"/>
-      </Head>
+      <title>Inicio | move.it</title>
+    </Head>
       <ExperienceBar />
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges/>
+          <Countdown/>
+        </div>
+        <div>
+
+        </div>
+      </section>
     </div>
   )
 }
